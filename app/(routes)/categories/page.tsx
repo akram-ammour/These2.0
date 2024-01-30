af906b55b8d4cc3b85bd29e5172a13aa082f4c14
akram-ammour <akram.ammour.91@gmail.com>
2024-01-30 13:17:56 +0100

    removed bugs

--- app/(routes)/categories/page.tsx
+++ app/(routes)/categories/page.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 type Props = {};
 
 const Page = async (props: Props) => {
-  const {isLoading,error,success,categories} = await getAllCategories()
+  const {categories} = await getAllCategories()
 
   // useEffect(() => {
   //   startTransition(() => {
@@ -33,24 +33,24 @@ const Page = async (props: Props) => {
   // }, []);
 
 
-  if (error) {
-    return (
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-        <p className="text-lg min-w-40">
-          This is an Error contact us we would like it if we received your
-          feedback{": "}
-          <a
-            href={`mailto:akram.ammour.mail@gmail.com?subject=From%20these2.0&body=error from getAllCategories() with the error ${JSON.stringify(
-              error
-            )}`}
-            className="font-bold underline"
-          >
-            Learn more
-          </a>
-        </p>
-      </div>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+  //       <p className="text-lg min-w-40">
+  //         This is an Error contact us we would like it if we received your
+  //         feedback{": "}
+  //         <a
+  //           href={`mailto:akram.ammour.mail@gmail.com?subject=From%20these2.0&body=error from getAllCategories() with the error ${JSON.stringify(
+  //             error
+  //           )}`}
+  //           className="font-bold underline"
+  //         >
+  //           Learn more
+  //         </a>
+  //       </p>
+  //     </div>
+  //   );
+  // }
 
   return <div className="px-8 py-12">
     <CategoryCardsLister categories={categories}/>
