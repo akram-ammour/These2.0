851f3a8b4100c227073c88034ca2c0b51fbee1bf
akram-ammour <akram.ammour.91@gmail.com>
2024-02-12 15:15:12 +0100

    test react-pdf

--- components/pdf-full-screen.tsx
+++ components/pdf-full-screen.tsx
@@ -24,50 +24,48 @@ const PdfFullScreen = ({ href, title }: Props) => {
   const { width, ref } = useResizeDetector();
 
   return (
-    <Dialog
-    open={isOpen}
-    onOpenChange={(v) => setIsOpen(v)}
-  >
-    <DialogTrigger onClick={() => setIsOpen(true)} asChild>
-      <Button variant="ghost" size="icon" className="text-gray-600">
-        <Expand className="h-6 w-6" />
-      </Button>
-    </DialogTrigger>
-    <DialogContent className="max-w-7xl w-full h-[90vh]">
-      <SimpleBar
-        className="max-h-[calc(100vh-10rem)] mt-6"
-        style={{ overflowY: "scroll" }}
-      >
-        <div ref={ref} className="overflow-hidden">
-          <Document
-            loading={
-              <div className="flex justify-center">
-                <Loader2 className="my-24 h-6 w-6 animate-spin" />
-              </div>
-            }
-            onLoadError={() => {
-              toast({
-                title: "Error loading PDF",
-                description: "Please try again later",
-                variant: "destructive",
-              });
-            }}
-            onLoadSuccess={({ numPages }) => {
-              console.log(numPages);
-              setNumPages(numPages);
-            }}
-            file={href}
-            className="max-h-full overflow-hidden"
-          >
-            {numPages &&
-              Array.from({ length: numPages }, (_, i) => (
-                <Page key={i} width={width ? width : 1} pageNumber={i + 1} />
-              ))}
-          </Document>
-        </div>
-      </SimpleBar>
-    </DialogContent>
-  </Dialog>
-);
+    <Dialog open={isOpen} onOpenChange={(v) => setIsOpen(v)}>
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button variant="ghost" size="icon" className="text-gray-600">
+          <Expand className="h-6 w-6" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-7xl w-full h-[90vh]">
+        <SimpleBar
+          className="max-h-[calc(100vh-10rem)] mt-6"
+          style={{ overflowY: "scroll" }}
+        >
+          <div ref={ref} className="overflow-hidden">
+            <Document
+              loading={
+                <div className="flex justify-center">
+                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: "Error loading PDF",
+                  description: "Please try again later",
+                  variant: "destructive",
+                });
+              }}
+              onLoadSuccess={({ numPages }) => {
+                console.log(numPages);
+                setNumPages(numPages);
+              }}
+              file={href}
+              className="max-h-full overflow-hidden"
+            >
+              {/* test */}
+              {numPages &&
+                Array.from({ length: numPages }, (_, i) => (
+                  <Page key={i} width={width ? width : 1} pageNumber={i + 1} />
+                ))}
+            </Document>
+          </div>
+        </SimpleBar>
+      </DialogContent>
+    </Dialog>
+  );
 };
 export default PdfFullScreen;
