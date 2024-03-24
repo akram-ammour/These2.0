f01001aa1b48ac5d38873112c9bf1ea5f18a8b68
akram-ammour <akram.ammour.91@gmail.com>
2024-03-24 23:56:40 +0000

    added api/renew-data route

--- app/api/renew-data/route.ts
+++ app/api/renew-data/route.ts
@@ -1,13 +1,12 @@
-import { renewData } from '@/actions/renew-data'
-import { NextRequest, NextResponse } from 'next/server'
+import { renewData } from "@/actions/renew-data";
+import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET(request: NextRequest) {
-    const {success,error} = await renewData()
-    if(error){
-        return new Response('Couldn\'t renew data')
-    }
+  const { success, error } = await renewData();
+  if (error) {
+    return new Response("Couldn't renew data");
+  }
 
-
-    return new Response('data renewed')
-
+  return new Response("data renewed");
 }
