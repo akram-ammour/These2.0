90aca4fd8e606f8cec3bb60f002bedfeaa645871
akram-ammour <akram.ammour.91@gmail.com>
2024-03-25 00:08:37 +0000

    added api/renew-data route

--- app/api/renew-data/route.ts
+++ app/api/renew-data/route.ts
@@ -1,6 +1,7 @@
 import { renewData } from "@/actions/renew-data";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "edge"; // 'nodejs' is the default
 export const dynamic = "force-dynamic";
 export async function GET(request: NextRequest) {
   const { success, error } = await renewData();
